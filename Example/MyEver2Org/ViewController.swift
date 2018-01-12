fd599e76021e7b917ac32027f4740a4a908d644c
Stephen Switzer <sdamnit@sdamnit.com>
2018-01-12 12:34:27 -0800

    Initial commit

---
+++ Example/MyEver2Org/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyEver2Org
+//
+//  Created by sdamnit on 01/12/2018.
+//  Copyright (c) 2018 sdamnit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
